8c74fa90df97c120bffad8266a40dca64c08f7b4
waitwalker <waitwalker@163.com>
2020-04-20 09:57:35 +0800

    初始化根控制

---
+++ FirePlayer/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  FirePlayer
+//
+//  Created by etiantian on 2020/4/20.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ FirePlayer/Main/MainWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  MainWindowController.swift
+//  FirePlayer
+//
+//  Created by etiantian on 2020/4/20.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+    }
+    
+}
